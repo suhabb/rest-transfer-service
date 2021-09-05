3d3c3e311e33a8605e4e41902733cadc416bcda5
suhailmir <suhabb@gmail.com>
2021-09-05 12:45:58 +0100

    Main file

---
+++ src/main/java/com/transfer/RestTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTransferServiceApplication.class, args);
+	}
+
+}
